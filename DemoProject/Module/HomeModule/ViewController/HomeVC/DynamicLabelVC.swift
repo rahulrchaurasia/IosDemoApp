b085389d33bae102e676107a10055d6c2ac807cd
Nilesh Birhade <rahulrchaurasia@gmail.com>
2021-12-12 00:43:01 +0530

    Adding Scroollview ui

--- DemoProject/Module/HomeModule/ViewController/HomeVC/DynamicLabelVC.swift
+++ DemoProject/Module/HomeModule/ViewController/HomeVC/DynamicLabelVC.swift
@@ -2,57 +2,51 @@
 //  DynamicLabelVC.swift
 //  DemoProject
 //
-//  Created by Daniyal Shaikh on 06/10/21.
+//  Created by Daniyal Shaikh on 11/12/21.
 //  Copyright © 2021 Nilesh Birhade. All rights reserved.
 //
 
-/*
-
- UIView dynamic height depending on Label Height :---
-https://stackoverflow.com/questions/39410796/uiview-dynamic-height-depending-on-label-height
- 
- 
- 
- Note : For Scrollview  :
- if scrollview content is more : we can set ◺ Simulated size : freedom and set required height
-  Case 1> set Container width equal to scrollview and give heght = required height
- 
- case 2 > set Container width equal to scrollview  and height constrain also
-          equal to scrollview but low priority ie 250
- */
 import UIKit
 
 class DynamicLabelVC: UIViewController {
-
-    @IBOutlet weak var Header1View: UIStackView!
-    @IBOutlet weak var Header2View: UIStackView!
-    @IBOutlet weak var Header3View: UIStackView!
     
+    @IBOutlet weak var headerView1: UIStackView!
+    
+    @IBOutlet weak var headerView2: UIStackView!
+    
+    
+    @IBOutlet weak var headerView3: UIStackView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
-        
-        //swift code in view controller
-
         let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.clickAction(sender:)))
-        self.Header1View.addGestureRecognizer(gesture)
-
-       
-
+        self.headerView1.addGestureRecognizer(gesture)
+        
     }
     
-
+    
     @objc func clickAction(sender : UITapGestureRecognizer) {
-       
         
-        let objVC =  ScrollDemoVC.shareInstance()
-            
+        let objVC = ScrollDemoVC.shareInstance()
+         
         navigationController?.pushViewController(objVC, animated: true)
+        
     }
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
+
 
 extension DynamicLabelVC {
     
